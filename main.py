5526b0599ce406d99621f697273027815a754c07
sarthakmalikmeerut <66469025+sarthakmalikmeerut@users.noreply.github.com>
2023-04-18 21:31:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,23 @@ import smtplib
 import cvzone
 import math
 
-vid = cv2.VideoCapture(0)
+vid = cv2.VideoCapture("C:\\Users\\sarth\\PycharmProjects\\yolo_project\\car_fire.mp4")
 vid.set(3,1280)
 vid.set(4,720)
 model = YOLO("C:\\Users\\sarth\\PycharmProjects\\yolo_project\\best.pt")
+
+
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
 
 
 while True:
@@ -26,12 +39,12 @@ while True:
             if conf > 0.7 :
                 s = smtplib.SMTP('smtp.gmail.com', 587)
                 s.starttls()
-                s.login("sarthakmalikmeerut@gmail.com", "zfvflbnkdxndohel")
+                s.login("sarthakmalikmeerut@gmail.com", "doomgqtqjkqoosyc")
                 message = "your location is jalandhar"
                 s.sendmail("sarthakmalikmeerut@gmail.com", "sarthakmalikmeerut@gmail.com", message)
             cls = int(box.cls[0])
 
-            cvzone.putTextRect(img,f'{conf}', (max(0,x1), max(35,y1)))
+            cvzone.putTextRect(img,f'{names[cls]} {conf}', (max(0,x1), max(35,y1)))
 
 
     cv2.imshow("image",img)
